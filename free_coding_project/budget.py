685d62b5228c15020aa83244592f8eda1504eda7
Rockylane01 <rockylane01@gmail.com>
2025-04-12 19:10:32 -0600

    Adding the ploting functionality and fixing some stuff.

--- free_coding_project/budget.py
+++ free_coding_project/budget.py
@@ -1,3 +1,6 @@
+
+import matplotlib.pyplot as plt
+
 class Budget:
 
     # Keeps track of the pas transaction so you can plot it later
@@ -16,18 +19,79 @@ class Budget:
     def addIncome(self, income, category, date):
         self.balance += income
 
-        self.transaction_history['income_value'].append(income)
-        self.transaction_history['income_category'].append(category)
-        self.transaction_history['income_date'].append(date)
+        if self.transaction_history["income_value"] == []:
+            self.transaction_history['income_value'].append(income)
+            self.transaction_history['income_category'].append(category)
+            self.transaction_history['income_date'].append(date)
+        else:
+            for i in range(len(list(self.transaction_history.values())[0])):
+                if self.transaction_history['income_date'][i] > date:
+                    self.transaction_history['income_value'].insert(i, income)
+                    self.transaction_history['income_category'].insert(i, category)
+                    self.transaction_history['income_date'].insert(i, date)
+
+        # print(self.transaction_history)
+        # print(self.balance)
+
 
     def addExpense(self, expense, category, date):
         self.balance -= expense
 
-        self.transaction_history['expense_value'].append(expense)
-        self.transaction_history['expense_category'].append(category)
-        self.transaction_history['expense_date'].append(date)
+        if self.transaction_history["expense_value"] == []:
+            self.transaction_history['expense_value'].append(expense)
+            self.transaction_history['expense_category'].append(category)
+            self.transaction_history['expense_date'].append(date)
+        else:
+            for i in range(len(self.transaction_history['expense_date'])):
+                if self.transaction_history['expense_date'][i] >= date:
+                    self.transaction_history['expense_value'].insert(i, expense)
+                    self.transaction_history['expense_category'].insert(i, category)
+                    self.transaction_history['expense_date'].insert(i, date)
+                    break
+
+        # print([x.day for x in self.transaction_history['expense_date']])
+        # print(self.balance)
 
     def plotExpenses(self):
+        X = [x.day for x in self.transaction_history['expense_date']]
+        Y = self.transaction_history['expense_value']
+        print(X, Y)
+
+        plt.plot(X, Y)
+        plt.xlabel('Day of month')
+        plt.ylabel('Expense amount in USD')
+        plt.title('Expense overview')
+        plt.savefig("data_vis/expense_plot.png")
+
+    def exp_category_perc(self):
+        cat_values = {}
+        # sum_expenses = {}
+        total_expense = 0
+        # cat_perc = {}
+        categories = []
+        cat_perc = []
+        
+        print(self.transaction_history['expense_category'])
+        for i in range(len(self.transaction_history['expense_category'])):
+            category = self.transaction_history['expense_category'][i]
+            expense = self.transaction_history['expense_value'][i]
+            if category in cat_values:
+                cat_values[category].append(expense)
+            else:
+                cat_values[category] = [expense]
+        
+        for key in cat_values:
+            total_expense += sum(cat_values[key])
+            # cat_perc[key] = int((sum(cat_values[key]) / total_expense) * 100)
+
+        for key in cat_values:
+            categories.append(key)
+            cat_perc.append((sum(cat_values[key]) / total_expense) * 100)
+        
+        plt.pie(cat_perc, labels=categories)
+        plt.savefig("data_vis/expense_pie.png")
+
+    def pieExpences(self):
         pass
 
     def plotExpenseDistribution(self):
--- free_coding_project/run.py
+++ free_coding_project/run.py
@@ -1,5 +1,7 @@
 from account import *
 from transaction import *
+from budget import *
+from datetime import datetime
 
 # takes a dict of transactions and their amounts
 def find_categories_amount(transactions):
@@ -14,10 +16,24 @@ def find_categories_amount(transactions):
 
 
 if __name__ == "__main__":
-    free_checking = Account("Free Checking", "checking", 300)
-    free_checking.withdraw(125)
+    free_checking = Account("Free Checking", "checking", 1000)
+    free_checking.deposit(125)
+
+    my_budget = Budget(1000)
+    my_budget.addIncome(500, "Pay Check", datetime(2025,4,10))
+    my_budget.addExpense(100, "Groceries", datetime(2025, 4, 10))
+    my_budget.addExpense(30, "Recreation", datetime(2025, 4, 5))
+    my_budget.addExpense(500, "Rent", datetime(2025, 4, 1))
+    my_budget.addExpense(30, "Gas", datetime(2025, 4, 8))
+    my_budget.addExpense(24, "Eating out", datetime(2025, 4, 9))
+    my_budget.addExpense(10, "Eating out", datetime(2025, 4, 7))
+    my_budget.addExpense(15, "Eating out", datetime(2025, 4, 8))
+
+    # my_budget.plotExpenses()
+    my_budget.exp_category_perc()
+
     # print(free_checking)
-    trans1 = Transaction("Walmart", 200, free_checking, "Groceries", "hahaha")
-    trans2 = Transaction("Peak Nights", 30, free_checking, "Recreational", "hahaha")
-    print(trans1)
-    print(find_categories_amount([trans1, trans2]))
+    # trans1 = Transaction("Walmart", 200, free_checking, "Groceries", "hahaha")
+    # trans2 = Transaction("Peak Nights", 30, free_checking, "Recreational", "hahaha")
+    # print(trans1)
+    # print(find_categories_amount([trans1, trans2]))
